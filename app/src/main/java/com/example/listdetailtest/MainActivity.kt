806b9457414eef19a8fd7ac1b53fffb87bfb61de
kubag <jakubgr02@gmail.com>
2024-05-09 22:15:30 +0200

    Add photos

--- app/src/main/java/com/example/listdetailtest/MainActivity.kt
+++ app/src/main/java/com/example/listdetailtest/MainActivity.kt
@@ -6,7 +6,7 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.BackHandler
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.detectHorizontalDragGestures
 import androidx.compose.foundation.layout.Arrangement
@@ -15,14 +15,15 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
-import androidx.compose.material3.ListItem
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.adaptive.ExperimentalMaterial3AdaptiveApi
@@ -42,10 +43,13 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.Job
@@ -119,24 +123,42 @@ fun TrailList(
     onItemClick: (TrailItem) -> Unit,
     items: List<Trail>
 ) {
-    Card {
-        LazyColumn {
-            items.forEachIndexed { id, trail ->
-                item {
-                    ListItem(
-                        modifier = Modifier
-                            .background(Color.Magenta)
-                            .clickable {
-                                onItemClick(TrailItem(id))
-                            },
-                        headlineContent = {
-                            Text(
-                                text = trail.name,
-                            )
-                        },
-                    )
-                }
+    LazyColumn {
+        items.forEachIndexed { _, trail ->
+            item {
+                TrailCard(trail, onItemClick)
             }
+        }
+    }
+}
+
+@Composable
+fun TrailCard(trail: Trail, onItemClick: (TrailItem) -> Unit) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp)
+            .clickable { onItemClick(TrailItem(trail.id)) }
+    ) {
+        Column(
+            modifier = Modifier.padding(8.dp)
+        ) {
+            Image(
+                painter = painterResource(photoList[trail.id]),
+                contentDescription = null,
+                modifier = Modifier
+                    .height(200.dp)
+                    .fillMaxWidth()
+                    .clip(shape = RoundedCornerShape(8.dp)),
+                contentScale = ContentScale.Crop
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(
+                text = trail.name,
+                style = MaterialTheme.typography.headlineMedium,
+                modifier = Modifier.padding(horizontal = 16.dp),
+                textAlign = TextAlign.Center
+            )
         }
     }
 }
@@ -328,3 +350,4 @@ fun formatTime(seconds: Int): String {
     return String.format("%02d:%02d:%02d", hours, minutes, remainingSeconds)
 }
 
+val photoList: Array<Int> = arrayOf(R.drawable.trail_image_0, R.drawable.trail_image_1)
